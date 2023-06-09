d080bf7b9ecf799eff5fa051f7e9fa731058632d
Kiran Kodali <kirankodali10@icloud.com>
2023-06-08 18:39:37 -0700

    data commented

--- backend-server2/DSM.py
+++ backend-server2/DSM.py
@@ -13,23 +13,5 @@ JOB_POSTS = [
         "jobDescription": "software developer",
         "companyName": "Google",
         "jobCategory": "DEVELOPMENT"
-    },
-    {
-        "jobTitle": "sde-2",
-        "jobDescription": "software developer",
-        "companyName": "Tesla",
-        "jobCategory": "DEVELOPMENT"
-    },
-    {
-        "jobTitle": "Analyst",
-        "jobDescription": "marketing analyst",
-        "companyName": "Microsoft",
-        "jobCategory": "MARKETING"
-    },
-    {
-        "jobTitle": "Analyst II",
-        "jobDescription": "marketing analyst",
-        "companyName": "HCL",
-        "jobCategory": "MARKETING"
-    },
+    }
 ]
